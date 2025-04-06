20a7884524adc9d5e0eeb937fc134ea1152662d1
Froggotkd <djojavi19@gmail.com>
2025-04-05 19:20:07 -0500

    Feat(loading): add loading page with custom animation

--- Hambrosia/components/Loading.tsx
+++ Hambrosia/components/Loading.tsx
@@ -1,31 +1,40 @@
 import React from 'react';
-import { View, StyleSheet, Dimensions } from 'react-native';
+import { View, StyleSheet, Dimensions, Text} from 'react-native';
 import LottieView from 'lottie-react-native';
 
 const { width } = Dimensions.get('window');
 
+
+
 export default function Loading() {
-  return (
-    <View style={styles.container}>
-      <LottieView
-        source={require('@/assets/images/animation.json')}
-        autoPlay
-        loop
-        style={styles.animation}
-      />
-    </View>
-  );
+
+    return (
+        <View style={styles.container}>
+            <LottieView
+                source={require('@/assets/images/animation.json')}
+                autoPlay
+                loop
+                style={styles.animation}
+            />
+            <Text style={styles.logo}>HAMBROSIA</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#E74C3C',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  animation: {
-    width: width * 0.6,
-    height: width * 0.6,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#E74C3C',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    animation: {
+        width: width * 0.6,
+        height: width * 0.6,
+    },
+    logo: {
+        fontSize: 35,
+        fontWeight: 'bold',
+        color: '#fff',
+    },
 });
